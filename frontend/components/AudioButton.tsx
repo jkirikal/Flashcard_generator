cb2657a7526c79f64f43f14e9098f60379dc4adc
d4gl4s <daglas.aitsen@gmail.com>
2024-05-26 14:18:12 +0300

    ability to play questions and answers with generated audio

--- frontend/components/AudioButton.tsx
+++ frontend/components/AudioButton.tsx
@@ -1,19 +1,23 @@
 "use client"
 
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { FaPlay, FaSquareFull } from "react-icons/fa6"
+import { ScaleLoader } from "react-spinners"
 
 interface AudioButtonProps {
-  src: string
+  text: string
 }
 
-const AudioButton: React.FC<AudioButtonProps> = ({ src }) => {
+const AudioButton: React.FC<AudioButtonProps> = ({ text }) => {
   const audioRef = useRef<HTMLAudioElement>(null)
   const [playing, setPlaying] = useState<boolean>(false)
+  const [audioSrc, setAudioSrc] = useState<string>("")
+  const [loading, setLoading] = useState(false)
 
   const playAudio = () => {
     if (audioRef.current) {
       audioRef.current.play()
+      setLoading(false)
       setPlaying(true)
     }
   }
@@ -26,10 +30,45 @@ const AudioButton: React.FC<AudioButtonProps> = ({ src }) => {
     }
   }
 
+  const fetchAudio = async () => {
+    setLoading(true)
+    const payload = { text: text, speaker: "Albert" }
+    const headers = { "Content-Type": "application/json" }
+    try {
+      const response = await fetch("https://api.tartunlp.ai/text-to-speech/v2", {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(payload),
+      })
+      const data = await response.arrayBuffer()
+      const audioBlob = new Blob([data], { type: "audio/ogg" })
+      const audioUrl = URL.createObjectURL(audioBlob)
+      setAudioSrc(audioUrl)
+      if (audioRef.current) {
+        audioRef.current.src = audioUrl
+        playAudio()
+      }
+    } catch (error) {
+      console.error("Error fetching audio:", error)
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    const audioElement = audioRef.current
+    const handleEnded = () => setPlaying(false)
+
+    if (audioElement) audioElement.addEventListener("ended", handleEnded)
+
+    return () => {
+      if (audioElement) audioElement.removeEventListener("ended", handleEnded)
+    }
+  }, [])
+
   return (
     <div>
-      <button onClick={() => (playing ? stopAudio() : playAudio())} className="bg-indigo-400 h-12 w-12 flex justify-center items-center rounded-[50px]">
-        {playing ? <FaSquareFull className="text-white" size={12} /> : <FaPlay className="text-white ml-1" />}
+      <button onClick={() => (playing ? stopAudio() : fetchAudio())} className="bg-indigo-400 h-12 w-12 flex justify-center items-center rounded-[50px]">
+        {playing ? <FaSquareFull className="text-white" size={12} /> : loading ? <ScaleLoader color="white" height={15} width={3} margin={1} /> : <FaPlay className="text-white ml-1" />}
       </button>
       <audio ref={audioRef} src={"/example_bossa_nova.ogg"} />
     </div>
--- frontend/components/FlashcardCard.tsx
+++ frontend/components/FlashcardCard.tsx
@@ -1,10 +1,14 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import AudioButton from "./AudioButton"
 import MicrophoneRecorder from "./MicrophoneRecorder"
 import { FaEye, FaEyeSlash } from "react-icons/fa"
 
 const FlashcardCard = ({ flashcard, index }: { flashcard: flashcardType; index: number }) => {
   const [revealed, setRevealed] = useState(false)
+
+  useEffect(() => {
+    setRevealed(false)
+  }, [flashcard])
 
   return (
     <div className="rounded-2xl bg-white">
@@ -18,7 +22,7 @@ const FlashcardCard = ({ flashcard, index }: { flashcard: flashcardType; index: 
           ) : (
             <FaEye className="mr-4 text-slate-700 cursor-pointer" onClick={() => setRevealed(true)} size={24} />
           )}
-          <AudioButton src="/frontend/public/example_bossa_nova.ogg" />
+          <AudioButton text={revealed ? flashcard.answer : flashcard.question} />
         </div>
       </div>
       <div onClick={() => setRevealed(!revealed)} className="bg-white pb-24 pt-8 w-full mb-4 px-8 cursor-pointer rounded-2xl min-h-[17em] flex items-center justify-center">
